fe8aeb1c3ad5994363def68546b242c67d715fcd
Saeed Marzban <Saeed.mb8@gmail.com>
2019-07-03 13:02:02 +0200

    change molo.surveys to molo.forms

---
+++ local_test_settings.py
@@ -0,0 +1,21 @@
+from .base import *  # noqa: F403
+
+
+INSTALLED_APPS += (
+    'wagtail_personalisation',
+    'wagtailfontawesome',
+    'wagtailsurveys',
+)
+
+CELERY_ALWAYS_EAGER = True
+BROKER_BACKEND = 'memory'
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '.localhost',
+    'site2',
+]
+
+PERSONALISATION_SEGMENTS_ADAPTER = (
+    'molo.forms.adapters.PersistentSurveysSegmentsAdapter'
+)
