7e76b16cb25e4c2a8dd5aefed5138dd12edacc60
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-06-04 08:55:51 +0200

    fix template tag bug

--- molo/forms/templatetags/molo_forms_tags.py
+++ molo/forms/templatetags/molo_forms_tags.py
@@ -37,30 +37,26 @@ def get_form_list(
                 language__is_main_language=True,
                 display_form_directly=False,
                 contact_form=False,
-                article_form_only=True,
                 your_words_competition=False
             ).exact_type(MoloFormPage).specific())
         elif only_direct_forms:
             forms = (MoloFormPage.objects.descendant_of(page).filter(
                 language__is_main_language=True, display_form_directly=True,
-                your_words_competition=False, article_form_only=False,
+                your_words_competition=False,
             ).exact_type(MoloFormPage).specific())
         elif only_yourwords:
             forms = (MoloFormPage.objects.descendant_of(page).filter(
                 language__is_main_language=True,
                 your_words_competition=True,
-                article_form_only=False
             ).exact_type(MoloFormPage).specific())
         elif personalisable_form:
             forms = (PersonalisableForm.objects.descendant_of(page).filter(
                 language__is_main_language=True,
-                article_form_only=False
             ).exact_type(PersonalisableForm).specific())
 
         elif contact_form:
             forms = (MoloFormPage.objects.descendant_of(page).filter(
                 language__is_main_language=True,
-                article_form_only=False,
                 contact_form=True).exact_type(
                 MoloFormPage).specific())
 
